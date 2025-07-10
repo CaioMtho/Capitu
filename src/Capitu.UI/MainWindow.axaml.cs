192561e00e4457d819ba41988ef4fc6f4903a2d3
CaioMtho <caiomathol@gmail.com>
2025-07-10 11:58:27 -0300

    feat(UI): filepicker na interface

--- src/Capitu.UI/MainWindow.axaml.cs
+++ src/Capitu.UI/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
 
 namespace Capitu.UI;
 
@@ -7,5 +11,32 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private async void LoadFileBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var path = await GetFilePath();
+        StatusText.Text = "Caminho do arquivo: " + path;
+        
+    }
+
+    private async Task<string?> GetFilePath()
+    {
+        var topLevel = GetTopLevel(this) ?? throw new InvalidOperationException("Erro de TopLevel");
+        var file = await topLevel.StorageProvider.OpenFilePickerAsync(
+            new FilePickerOpenOptions
+            {
+                Title = "Selecione um arquivo para diagramação",
+                AllowMultiple = false,
+                FileTypeFilter =
+                [
+                    new FilePickerFileType("Textos")
+                    {
+                        Patterns = ["*.txt", "*.pdf", "*.docx"]
+                    }
+                ]
+            });
+        
+        return file.Count > 0 ? file[0].Path.LocalPath : null;
     }
 }
